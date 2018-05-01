d9fe3dc9f7c87efe24239e9a411579a27c1bfd01
Chase <hmmChase@users.noreply.github.com>
2018-04-30 20:45:51 -0600

    Add district data to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import DistrictRepository from '../../helper';
-import kinderData from '../../data/kindergartners_in_full_day_program'
+import kinderData from '../../data/kindergartners_in_full_day_program';
 import './App.css';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
+
+    this.state = {
+      foundDistricts: []
+    };
   }
 
   componentDidMount() {
-    const district = new DistrictRepository(kinderData)
-    console.log(district)
+    const district = new DistrictRepository(kinderData);
+    const foundDistricts = district.findAllMatches();
+    this.setState({
+      foundDistricts
+    });
   }
 
   render() {
-    return (
-      <div>Welcome To Headcount 2.0</div>
-    );
+    return <div>Welcome To Headcount 2.0</div>;
   }
 }
 
