a0bb6290f7d805afaec76b6f0b3998e5b8ecc5e7
Chase <hmmChase@users.noreply.github.com>
2018-04-30 21:08:00 -0600

    Render DistrictContainer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DistrictRepository from '../../helper';
 import kinderData from '../../data/kindergartners_in_full_day_program';
+import DistrictContainer from '../DistrictContainer/DistrictContainer';
 import './App.css';
 
 class App extends Component {
@@ -21,7 +22,12 @@ class App extends Component {
   }
 
   render() {
-    return <div>Welcome To Headcount 2.0</div>;
+    return (
+      <div>
+        <h1>Welcome To Headcount 2.0</h1>
+        <DistrictContainer foundDistricts={this.state.foundDistricts} />
+      </div>
+    );
   }
 }
 
