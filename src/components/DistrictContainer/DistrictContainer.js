d56862c91b96507e214e2afc637bcf2dce008719
Chase <hmmChase@users.noreply.github.com>
2018-04-30 21:07:12 -0600

    Create DistrictContainer component

---
+++ src/components/DistrictContainer/DistrictContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Card from '../Card/Card';
+import './DistrictContainer.css';
+
+const DistrictContainer = props => {
+  return <Card />;
+};
+
+export default DistrictContainer;
---
+++ src/components/DistrictContainer/DistrictContainer.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import DistrictContainer from './DistrictContainer';
+
+describe('DistrictsContainer', () => {
+  it('', () => {
+    expect();
+  });
+});
