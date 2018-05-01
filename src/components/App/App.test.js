ef7af3e6591cdc73b97f41bff5cb41cb41fbab96
Cameron Buscher <cameronbuscher@gmail.com>
2018-05-01 15:32:51 -0600

    Add tests for app
    Including filterDistricts function and componentDidMOunt setting out state

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import kinderData from '../../data/kindergartners_in_full_day_program';
+
+import {shallow} from 'enzyme'
 
 describe('App', () =>{
-  beforeEach(() => {
-    const app = shallow(<App />)
-  })
-  describe('componentDidMount', () => {
+  let app
 
+  beforeEach(() => {
+    app = shallow(<App />, { disableLifecycleMethods: true })
+  })
+  
+  it('matches snapshot', () => {
+    expect(app).toMatchSnapshot()
+  })
+
+  describe('componentDidMount', () => {
+    it('sets app state', () => {
+      app.instance().componentDidMount()
+
+      expect(app.state().foundDistricts).toHaveLength(181)
+    })
+  })
+
+  describe('filterDistricts', () => {
+    it('filters districts and is not case sensitive', () => {
+      app.instance().filterDistricts('colo')
+
+      expect(app.state().foundDistricts).toHaveLength(2)
+    })
   })
 })
