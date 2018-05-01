77c7ff71c06ec31be6e6d3f2cb148ea9b2b2cf67
Chase <hmmChase@users.noreply.github.com>
2018-04-30 21:25:14 -0600

    Map over found districts and create a card component of each one, then render the cards

--- src/components/DistrictContainer/DistrictContainer.js
+++ src/components/DistrictContainer/DistrictContainer.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Card from '../Card/Card';
 import './DistrictContainer.css';
 
-const DistrictContainer = props => {
-  return <Card />;
+const DistrictContainer = ({ foundDistricts }) => {
+  const districtCards = foundDistricts.map((district, index) => {
+    return <Card key={`Card ${index}`} {...district} />;
+  });
+
+  return <section>{districtCards}</section>;
 };
 
 export default DistrictContainer;
