6b8a486e63a04f4a9d2d86a447d2a2a2ce7c845e
sospinar21 <sospinar21@gmail.com>
2018-05-02 14:43:21 -0600

    Add style to clicked card

--- src/components/DistrictContainer/DistrictContainer.js
+++ src/components/DistrictContainer/DistrictContainer.js
@@ -3,10 +3,17 @@ import Card from '../Card/Card';
 import PropTypes from 'prop-types'
 import './DistrictContainer.css';
 
-const DistrictContainer = ({ foundDistricts, changeSelectedDistricts }) => {
+const DistrictContainer = ({ foundDistricts, changeSelectedDistricts, comparedDistricts }) => {
   const districtCards = foundDistricts.map((district, index) => {
+    let clicked
+    if (comparedDistricts.includes(district)) {
+      clicked = true
+    } else {
+      clicked = false
+    }
     return (
       <Card
+        clicked={clicked}
         key={`Card ${index}`}
         {...district}
         changeSelectedDistricts={changeSelectedDistricts}
