d0d4f5ad4be2d760646354ff1d03a5fc63d4a407
Chase <hmmChase@users.noreply.github.com>
2018-05-01 19:54:11 -0600

    Create comparisonContainer component

---
+++ src/components/ComparisonContainer/ComparisonContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ComparisonContainer.css';
+
+const ComparisonContainer = (props) => {
+  return <article className="comparisonContainer">compared</article>;
+};
+
+export default ComparisonContainer;
