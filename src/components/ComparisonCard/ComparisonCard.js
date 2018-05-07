545ff2558bc2d85b0ac49153ddab29ff4aa97ea1
hmmChase <hmmchase@gmail.com>
2018-05-07 08:46:07 -0600

    Remove unnecessary spaces

--- src/components/ComparisonCard/ComparisonCard.js
+++ src/components/ComparisonCard/ComparisonCard.js
@@ -6,8 +6,8 @@ const ComparisonCard = ({ comparisonObject }) => {
   const keys = Object.keys(comparisonObject);
   return (
     <article className="comparisonCard">
-      <h4>{keys[0]} : {comparisonObject[keys[0]]}{' '}</h4>
-      <h4>{keys[1]} : {comparisonObject[keys[1]]}{' '}</h4>
+      <h4>{keys[0]} : {comparisonObject[keys[0]]}</h4>
+      <h4>{keys[1]} : {comparisonObject[keys[1]]}</h4>
       <h3>Comparison: {comparisonObject[keys[2]]}</h3>
     </article>
   );
